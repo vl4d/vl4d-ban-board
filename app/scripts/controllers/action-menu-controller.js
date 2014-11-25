42c71e3cc37c6beea6dc7818a671fc024c2061d7
vl4d <murray.vladimir@gmail.com>
2014-11-24 23:39:06 -0500

    added mock data for columns and kanban layout + stylings

--- app/scripts/controllers/action-menu-controller.js
+++ app/scripts/controllers/action-menu-controller.js
@@ -9,12 +9,13 @@
  */
 app.controller('ActionMenuCtrl', function($scope, $mdBottomSheet) {
   //Items that will show on the Action menu that pops up.
+  //TODO Add tooltip to template (or make my own).
   $scope.action_items = [
-    { name: 'Create',   icon: 'images/ic_grey-create.png' },
-    { name: 'Edit',     icon: 'images/ic_grey-edit.png'   },
-    { name: 'Load',     icon: 'images/ic_grey-load.png'   },
-    { name: 'Save',     icon: 'images/ic_grey-save.png'   },
-    { name: 'Share',    icon: 'images/ic_grey-share.png'  }
+    { name: 'Create',   icon: 'images/ic_grey-create.png',  tooltip_msg: 'Create... ' },
+    { name: 'Edit',     icon: 'images/ic_grey-edit.png',    tooltip_msg: 'Edit...' },
+    { name: 'Load',     icon: 'images/ic_grey-load.png',    tooltip_msg: 'Load from file' },
+    { name: 'Save',     icon: 'images/ic_grey-save.png',    tooltip_msg: 'Save to file' },
+    { name: 'Share',    icon: 'images/ic_grey-share.png',   tooltip_msg: 'Share Vl4d-Ban-Board' }
   ];
 
   $scope.listItemClick = function($index) {
--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -2,13 +2,36 @@
 
 /**
  * @ngdoc function
- * @name vl4dBanBoardApp.controller:MainCtrl
+ * @name vl4dBanBoardApp.controller:HomeCtrl
  * @description
- * # MainCtrl
- * Controller of the vl4dBanBoardApp
+ * # HomeCtrl
+ * Controller of the homepage for the vl4d-ban-board app
  */
 app.controller('HomeCtrl', function ($scope, $mdBottomSheet) {
-  $scope.hello = "Hello World";
+  //Mock Data - TODO Pull From File
+  $scope.kanban_cols =  [
+                          {
+                            title: "TODO",
+                            cards: [{title:"Learn Angular",details:"learn angularJS and make an app with it.",date_created:"2014-11-20"},
+                                    {title:"Learn Mongo DB",details:"Use mongodb in a project",date_created:"2014-11-10"},
+                                    {title:"Investigate Kanban Capabilities",details:"Figure out a Work Flow with a Kanban",date_created:"2014-11-19"}
+                                   ] //Array ofcards
+                          },
+                          {
+                            title: "Planned",
+                            cards: [{title:"Planned1",details:"planned details 1",date_created:"2014-11-19"},
+                                    {title:"Planned2",details:"planned details 2",date_created:"2014-10-20"},
+                                    {title:"Planned3",details:"planned details 3",date_created:"2014-10-14"},
+                                    {title:"Planned4",details:"planned details 4",date_created:"2014-10-12"}
+                                   ] //Array ofcards
+                          },
+                          {
+                            title: "In Progress",
+                            cards: [{title:"In Progress One",details:"In Progress Details One",date_created:"2014-09-10"},
+                              {title:"In Progress Two",details:"In Progress Details Two",date_created:"2014-09-11"}] //Array ofcards
+                          }
+
+  ];
 
   //Action menu that pops up when the upward icon is clicked.
   $scope.showActionMenu = function($event){
